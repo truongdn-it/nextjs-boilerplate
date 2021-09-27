f37649612f21b4e5720c5c1f7812f296aec925c4
Trường DN - F1 - D1 <truongdn@fabbi.io>
2021-09-27 18:01:04 +0700

    build: release nextjs base v1.0.0

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
